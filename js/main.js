26969f455b4586c657043381ccdf0921a290cb9d
floriantopf <florian.topf@gmail.com>
2016-01-05 00:15:05 +0100

    removed comments, formatted code

--- js/main.js
+++ js/main.js
@@ -1,16 +1,16 @@
 $(document).ready(function () {
     // init landing image size
-	setLandingImageHeight();
+    setLandingImageHeight();
 
-	// resize landing image on window resize
+    // resize landing image on window resize
 	$(window).on('resize', function () {
         setLandingImageHeight()
     });
 
-	function setLandingImageHeight () {
-		var wHeight = $(window).height();
-		var navHeight = $('.navigation').outerHeight(true);
-		var slideHeight = wHeight - navHeight;
-		$('.landing-slide').height(slideHeight);
-	}
+    function setLandingImageHeight () {
+        var wHeight = $(window).height();
+        var navHeight = $('.navigation').outerHeight(true);
+        var slideHeight = wHeight - navHeight;
+        $('.landing-slide').height(slideHeight);
+    }
 });
