b51d73cc1a29b4c143978d46468edd9f3206fc7c
floriantopf <florian.topf@gmail.com>
2016-01-05 14:15:08 +0100

    some corrections for mobile

--- js/main.js
+++ js/main.js
@@ -1,4 +1,13 @@
 $(document).ready(function () {
+    // When ready...
+    window.addEventListener("load",function() {
+        // Set a timeout...
+        setTimeout(function(){
+            // Hide the address bar!
+            window.scrollTo(0, 1);
+        }, 0);
+    });
+
     // init landing image size
     setLandingImageHeight();
 
