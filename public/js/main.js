894794a5b175ecf43e92d4ab289ca381634c9568
Florian Topf <florian.topf@gmail.com>
2021-06-26 21:19:23 +0200

    chore: cleanup styles and remove jquery (!)

--- public/js/main.js
+++ public/js/main.js
@@ -1,23 +1,11 @@
-$(document).ready(function () {
-    // When ready...
-    window.addEventListener("load",function() {
-        // Set a timeout...
-        setTimeout(function(){
-            // Hide the address bar!
-            window.scrollTo(0, 1);
-        }, 0);
-    });
-
-    // init landing image size
-    setLandingImageHeight();
-
-    // resize landing image on window resize
-    $(window).on('resize', setLandingImageHeight);
+(function (windowRef, documentRef) {
+    windowRef.addEventListener('load', setLandingImageHeight);
+    windowRef.addEventListener('resize', setLandingImageHeight);
 
     function setLandingImageHeight () {
-        const wHeight = $(window).height();
-        const navHeight = $('.navigation').outerHeight(true);
+        const wHeight = windowRef.innerHeight;
+        const navHeight = documentRef.getElementById('js-landing-header').offsetHeight;
         const slideHeight = wHeight - navHeight;
-        $('.landing-slide').height(slideHeight);
+        documentRef.getElementById('js-landing-slide').style.height = slideHeight + 'px';
     }
-});
+}(window, document));
